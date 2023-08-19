3350ee51e1cf500e5c589ccf072f286a45a6f0b6
EliasE83 <mayorelias83@gmail.com>
2023-08-18 20:16:41 -0600

    Archivo app y interfaz de login

---
+++ app.py
@@ -0,0 +1,25 @@
+#Importar el framework
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+#Inicializacion del APP
+app = Flask(__name__)
+
+#Configuracion de la conexion a la BD
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] =''
+app.secret_key = 'mysecretkey'
+mysql = MySQL(app)
+
+#Rutas
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
+#Ejecucion del Servidor en el puerto 5000
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
